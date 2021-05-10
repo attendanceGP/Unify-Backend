976b38507c292d61da1c6bd44cf053dfa7b09d9b
alialfie <alisamy27@gmail.com>
2021-05-11 00:13:36 +0200

    Update FirebaseMessagingResource.java

--- src/main/java/com/example/attendance/FirebaseMessaging/FirebaseMessagingResource.java
+++ src/main/java/com/example/attendance/FirebaseMessaging/FirebaseMessagingResource.java
@@ -4,6 +4,7 @@ import com.google.firebase.messaging.FirebaseMessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
@@ -19,7 +20,8 @@ public class FirebaseMessagingResource {
 
     @RequestMapping("/sendNotification")
     @ResponseBody
-    public String sendNotification() throws FirebaseMessagingException {
-        return firebaseMessagingService.sendNotification("Title", "body", "all");
+    public String sendNotification(@RequestParam String title, @RequestParam String body,
+                                   @RequestParam String topic) throws FirebaseMessagingException {
+        return firebaseMessagingService.sendNotification(title, body, topic);
     }
 }
