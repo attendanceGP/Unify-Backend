672a15c51e1c29277f533b4411aac15d5bec3395
youssef mohamed mohamed shehata <y0y01311999@gmail.com>
2021-07-14 19:18:16 +0200

    added more comments

--- src/main/java/com/example/attendance/Announcements/Repository/AnnouncementRepository.java
+++ src/main/java/com/example/attendance/Announcements/Repository/AnnouncementRepository.java
@@ -17,9 +17,11 @@ public interface AnnouncementRepository extends CrudRepository<Announcement, Int
     @Query(value = "DELETE from announcement WHERE id = :id ;", nativeQuery = true)
     void deleteById(@Param("id") Integer id);
 
+    //gets all announcements that a TA has posted according to their id
     @Query(value = "SELECT * from announcement WHERE fk_user_id = :userId ;", nativeQuery = true)
     List<Announcement> getTaAnnouncements(@Param("userId") Integer userId);
 
+    //gets all announcements for a student in the courses they are enrolled in
     @Query(value = "SELECT * FROM announcement WHERE fk_course_id IN (SELECT fk_course_code FROM user_course where fk_user_id = :userId) ;", nativeQuery = true)
     List<Announcement> getStudentAnnouncements(@Param("userId") Integer userId);
 
